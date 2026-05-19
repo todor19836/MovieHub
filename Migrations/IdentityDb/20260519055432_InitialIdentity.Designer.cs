66d49e1b81f21f095dce22c69247ac33358b7893
Nan3-1 <nataliqp.georgieva@gmail.com>
2026-05-19 09:15:51 +0300

    fixed the db

--- Migrations/IdentityDb/20260519055432_InitialIdentity.Designer.cs
+++ Migrations/IdentityDb/20260519055432_InitialIdentity.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieHub.Data;
 
 #nullable disable
 
-namespace MovieHub.Data.Migrations
+namespace MovieHub.Migrations.IdentityDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260519055432_InitialIdentity")]
+    partial class InitialIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
